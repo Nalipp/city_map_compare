100a6fb3029be4f3e74cb8d25c3b7c4f0d1e041d
Nalipp <nateplipp@gmail.com>
2018-10-12 13:08:42 -0700

    troubleshoot

--- app/utils/api.js
+++ app/utils/api.js
@@ -3,7 +3,7 @@ var MAPQUEST_API_KEY = require('../../.env').MAPQUEST_API_KEY;
 
 function getCoords(cityName) {
   return axios.get(
-    'http://www.mapquestapi.com/geocoding/v1/address?key=' + MAPQUEST_API_KEY + '=' + cityName)
+    'https://www.mapquestapi.com/geocoding/v1/address?key=' + MAPQUEST_API_KEY + '=' + cityName)
     .then(function(data) {
       return {
         coords : data.data.results[0].locations[0].latLng,
